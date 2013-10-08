9594d221b20c379f38bce98f9de4cab1fbd48b02
Mahendra M <mahendra.m@gmail.com>
2013-10-08 14:46:02 +0530

    Add proper string representations for each class

--- kafka/producer.py
+++ kafka/producer.py
@@ -207,6 +207,9 @@ class SimpleProducer(Producer):
     def __repr__(self):
         return '<SimpleProducer batch=%s>' % self.async
 
+    def __repr__(self):
+        return '<SimpleProducer topic=%s, batch=%s>' % (self.topic, self.async)
+
 
 class KeyedProducer(Producer):
     """
